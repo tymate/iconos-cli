c289c7c8dfee580a12e42b1e302855300d9d2871
Carquillat <benoit.carquillat@gmail.com>
2023-03-17 10:39:49 +0100

    si tu lis ce git commit -m "first commit" c'est que tu es un giga bg

---
+++ README.md
@@ -0,0 +1,22 @@
+# SVG to React Component CLI
+
+This command-line interface (CLI) tool converts SVG files into React components. It allows developers to easily import SVG icons into their React projects.
+
+## Installation
+
+To use this tool, you must first install it by running the following commands:
+
+```
+npm i
+npm install -g .
+```
+
+## Usage
+
+Once the tool is installed, you can run the following command in the folder containing your SVG files:
+
+```
+iconos
+```
+
+This command will convert all SVG files in the current folder into React components and save them in a new folder called Iconos.
---
+++ bin/index.mjs
@@ -0,0 +1,73 @@
+#!/usr/bin/env node
+
+import fs from "fs";
+import path from "path";
+import { transform } from "@svgr/core";
+import prettier from "prettier";
+
+const svgDir = "./";
+const componentsDir = "./components";
+
+// Vérifier si le dossier components existe, sinon le créer
+if (!fs.existsSync(componentsDir)) {
+  fs.mkdirSync(componentsDir);
+}
+
+fs.readdirSync(svgDir).forEach(async (file) => {
+  const filePath = path.join(svgDir, file);
+
+  // Vérifier si le fichier est un fichier SVG
+  if (path.extname(filePath).toLowerCase() === ".svg") {
+    const componentName = `${file.charAt(0).toUpperCase()}${file.slice(
+      1,
+      -4
+    )}`.replace(/-./g, (x) => x[1].toUpperCase());
+    const fileContent = fs.readFileSync(filePath, "utf8");
+
+    try {
+      const jsCode = await transform(
+        fileContent,
+        {
+          icon: true,
+          typescript: true,
+        },
+        { componentName: componentName }
+      );
+
+      // Ajouter la balise style avec les props passées
+      let fileContentProps = jsCode.replace(
+        /<svg([\s\S]*?)>/,
+        `<svg$1 style={{ ...props.style, color: props.color, fontSize: props.size }}>`
+      );
+
+      // Ajouter la balise style avec les props passées
+      fileContentProps = fileContentProps
+        .replace(/import \* as React from "react";\n/, "")
+        .replace(/\n/g, "\n\n");
+
+      console.log(`${componentName} ✅ - you're a giga bogoss`);
+
+      fs.writeFileSync(
+        path.join(componentsDir, `${componentName}.tsx`),
+        `${prettier.format(fileContentProps, {
+          printWidth: 90,
+          tabWidth: 2,
+          useTabs: false,
+          semi: true,
+          singleQuote: true,
+          trailingComma: "all",
+          bracketSpacing: true,
+          bracketLine: false,
+          jsxBracketSameLine: false,
+          arrowParens: "avoid",
+          requirePragma: false,
+          insertPragma: false,
+          proseWrap: "preserve",
+          parser: "typescript",
+        })}\n`
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  }
+});
